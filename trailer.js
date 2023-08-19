7426fb54b7c0d5f54a123368ca02546c974d0761
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-08-19 17:27:29 -0400

    Update trailer.js

--- trailer.js
+++ trailer.js
@@ -1,11 +1,6 @@
 var trailer;
 var x, y;
 
-window.onmousemove = e => {
-    x = e.clientX - trailer.offsetWidth / 2;
-    y = e.clientY - trailer.offsetHeight / 2;
-}
-
 function updateTrailer() {
     const keyframes = {
         transform: `translate(${x}px, ${y}px)`
@@ -27,6 +22,11 @@ window.addEventListener('load', e => {
     trailer = document.createElement("div");
     trailer.id = "trailer";
     trailerContainer.appendChild(trailer);
+
+    window.onmousemove = e => {
+        x = e.clientX - trailer.offsetWidth / 2;
+        y = e.clientY - trailer.offsetHeight / 2;
+    }
 
     requestAnimationFrame(updateTrailer);
 });
