c0ef34795c5a286ceeca467b2f837abf36f1bf0a
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-08-19 18:00:30 -0400

    tap tap

--- trailer.js
+++ trailer.js
@@ -14,7 +14,7 @@ function updateTrailer() {
     requestAnimationFrame(updateTrailer);
 }
 
-window.addEventListener('load', e => {
+document.addEventListener('load', e => {
     trailerContainer = document.createElement("div");
     trailerContainer.id = "trailer-container";
     document.body.appendChild(trailerContainer);
@@ -26,7 +26,12 @@ window.addEventListener('load', e => {
     requestAnimationFrame(updateTrailer);
 });
 
-document.addEventListener('onmousemove', e => {
-    x = e.clientX - trailer.offsetWidth / 2;
-    y = e.clientY - trailer.offsetHeight / 2;
-});
+window.onmousemove = e => {
+     x = e.clientX - trailer.offsetWidth / 2;
+     y = e.clientY - trailer.offsetHeight / 2;
+ }
+
+// document.addEventListener('onmousemove', e => {
+//     x = e.clientX - trailer.offsetWidth / 2;
+//     y = e.clientY - trailer.offsetHeight / 2;
+// });
