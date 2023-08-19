541d60f32e986b80a52fd9521c126a4fc31b4eea
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2023-08-19 18:21:58 -0400

    Update trailer.js

--- trailer.js
+++ trailer.js
@@ -15,15 +15,16 @@ function updateTrailer() {
 }
 
 window.addEventListener('load', e => {
-    console.log("loaded");
-    
+    // create container to prevent trailer from extending the window
     let trailerContainer = document.createElement("div");
     trailerContainer.id = "trailer-container";
     document.body.appendChild(trailerContainer);
-    
+
+    // add trailer to container
     trailer.id = "trailer";
     trailerContainer.appendChild(trailer);
 
+    // begin following mouse movements (should really append to event, but w/e)
     window.onmousemove = o => {
         x = o.clientX - trailer.offsetWidth / 2;
         y = o.clientY - trailer.offsetHeight / 2;
